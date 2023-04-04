9632e66e080ddf8d78e3f753b0fa72e530b26de2
Alekseymedvedev <61234001+Alekseymedvedev@users.noreply.github.com>
2023-04-04 18:01:20 +0300

    commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,37 +1,16 @@
-import React, {useEffect, useState} from 'react';
-import {BrowserRouter, redirect, Route, Routes, useLocation, useNavigate} from 'react-router-dom';
+import React, { useState} from 'react';
+import { Route, Routes, useNavigate} from 'react-router-dom';
 import {routes} from "./routes/routes";
 import '../shared/assets/styles/reset.scss';
 import '../shared/assets/styles/libs.scss';
 import '../shared/assets/styles/globals.scss';
-import {authSlice} from "../store/slice/authSlice";
-import {useAppDispatch, useAppSelector} from "../hooks/useRedux";
 import Auth from "../pages/auth";
 
 
 function App() {
-
-    const {auth} = authSlice.actions
-    const dispatch = useAppDispatch()
     const [userToken ,setUserToken] = useState<any>(localStorage.getItem('token'))
-    // const userToken = localStorage.getItem('token')
     const navigate = useNavigate()
-// refresh
-
-
-    console.log(1111)
     navigate(0)
-   useEffect(()=>{
-       setTimeout(()=>{
-           // setUserToken(localStorage.getItem('token'))
-
-       },3000)
-       if (userToken ) {
-           dispatch(auth(userToken))
-       } else {
-           dispatch(auth(userToken))
-       }
-   },[])
 
     return (
         <>
